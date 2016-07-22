82acf99f54344b0a0f98718ca40808ac4b851392
i40poster <kx.it4poster@gmail.com>
2016-07-21 23:11:21 -0300

    Deploy initial GeoFracker

---
+++ Docker/setupLibs.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+install.packages("rgeos")
+install.packages("rgdal")
+# raster install not working
+install.packages('raster',  repos='http://cran.us.r-project.org')
+install.packages('ncdf4')
+install.packages("xlsx")
+install.packages("ggmap")
+library(raster)
+library(rgdal)
